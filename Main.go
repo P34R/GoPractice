c4840ac523d713bb616f1aa88f6a1ccdb7d12f08
P34R <P34R@yandex.ru>
2021-10-31 09:29:41 +0200

    Queue functionality realization

--- Main.go
+++ Main.go
@@ -6,11 +6,22 @@ import (
 )
 
 func main() {
-	var myStack src.Stack
-	myStack.Push(1)
-	fmt.Println(myStack.Count())
-	fmt.Println(myStack.Peek())
-	fmt.Println(myStack.Pop())
-	fmt.Println(myStack.Pop())
-	fmt.Println(myStack.Count())
+	/*	var myStack src.Stack
+		myStack.Push(1)
+		fmt.Println(myStack.Count())
+		fmt.Println(myStack.Peek())
+		fmt.Println(myStack.Pop())
+		fmt.Println(myStack.Pop())
+		fmt.Println(myStack.Count())*/
+	var myQueue src.Queue
+	myQueue.Enqueue(5)
+	myQueue.Enqueue(3)
+	myQueue.Enqueue(1)
+	myQueue.Enqueue(4)
+	fmt.Println(myQueue.Count())
+	fmt.Println(myQueue.Peek())
+	fmt.Println(myQueue.Count())
+	fmt.Println(myQueue.Dequeue())
+	fmt.Println(myQueue.Count())
+	fmt.Println(myQueue.Peek())
 }
--- src/Queue.go
+++ src/Queue.go
@@ -5,14 +5,23 @@ type Queue struct {
 }
 
 func (f *Queue) Enqueue(value int) {
-
+	f.list = append(f.list, value)
+	f.list = append(f.list[len(f.list)-1:len(f.list)], f.list[0:len(f.list)-1]...)
 }
 func (f *Queue) Dequeue() int {
-
+	if len(f.list) == 0 {
+		return 0 //error, empty queue
+	}
+	last := f.list[len(f.list)-1]
+	f.list = append(f.list[0 : len(f.list)-1])
+	return last
 }
 func (f *Queue) Peek() int {
-
+	if len(f.list) == 0 {
+		return 0 //error, empty queue
+	}
+	return f.list[len(f.list)-1]
 }
 func (f *Queue) Count() int {
-
+	return len(f.list)
 }
