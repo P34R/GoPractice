87d287d94407a98b2019bd1d407b68f74b4c9a5f
P34R <P34R@yandex.ru>
2021-10-31 07:42:00 +0200

    Functionality realization

--- src/Stack.go
+++ src/Stack.go
@@ -5,14 +5,23 @@ type Stack struct {
 }
 
 func (f *Stack) Push(value int) {
-
+	f.list = append(f.list, value)
 }
-func (f *Stack) Pop() {
-
+func (f *Stack) Pop() int {
+	if len(f.list) == 0 {
+		return -1
+	}
+	var lastElement int
+	lastElement = f.list[len(f.list)-1]
+	f.list = append(f.list[:len(f.list)-1], f.list[len(f.list):]...)
+	return lastElement
 }
-func (f *Stack) Peek() {
-
+func (f *Stack) Peek() int {
+	if len(f.list) == 0 {
+		return -1
+	}
+	return f.list[len(f.list)-1]
 }
-func (f *Stack) Count() {
-
+func (f *Stack) Count() int {
+	return len(f.list)
 }
