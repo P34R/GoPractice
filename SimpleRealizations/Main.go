ae93148d5c921ae63808fda02eff12fd90aa3394
P34R <P34R@yandex.ru>
2021-12-28 23:31:01 +0200

    Queue implementation with linked list

--- SimpleRealizations/Main.go
+++ SimpleRealizations/Main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"GoTask1/src"
+	"GoTask1/SimpleRealizations/src"
 	"fmt"
 )
 
 func main() {
-	var myStack src.Stack
+	/*var myStack src.Stack
 	myStack.Push(1)
 	fmt.Println("count - ", myStack.Count())
 	myStack.Push(2)
@@ -18,18 +18,18 @@ func main() {
 	fmt.Println(myStack.Pop())
 	fmt.Println("count - ", myStack.Count())
 	fmt.Println(myStack.Pop())
-	fmt.Println("count - ", myStack.Count())
-	/*var myQueue src.Queue
-	myQueue.Enqueue(5)
-	myQueue.Enqueue(3)
+	fmt.Println("count - ", myStack.Count())*/
+	var myQueue src.Queue
 	myQueue.Enqueue(1)
+	myQueue.Enqueue(2)
+	myQueue.Enqueue(3)
 	myQueue.Enqueue(4)
-	fmt.Println(myQueue.Count())
-	fmt.Println(myQueue.Peek())
-	fmt.Println(myQueue.Count())
-	fmt.Println(myQueue.Dequeue())
-	fmt.Println(myQueue.Count())
-	fmt.Println(myQueue.Peek())*/
+	fmt.Println("size", myQueue.Count())
+	fmt.Println("peek", myQueue.Peek())
+	fmt.Println("size", myQueue.Count())
+	fmt.Println("deq", myQueue.Dequeue())
+	fmt.Println("size", myQueue.Count())
+	fmt.Println("peek", myQueue.Peek())
 	/*var myDeque src.Deque
 	myDeque.EnqueueFirst(1)
 	myDeque.EnqueueFirst(2)
