9d15caede82f78f1c67053030aa9e58183f90dd1
P34R <P34R@yandex.ru>
2021-12-29 00:04:48 +0200

    Deque implementation with doubly linked list

--- SimpleRealizations/Main.go
+++ SimpleRealizations/Main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	/*var myStack src.Stack
+	var myStack src.Stack
 	myStack.Push(1)
 	fmt.Println("count - ", myStack.Count())
 	myStack.Push(2)
@@ -18,7 +18,7 @@ func main() {
 	fmt.Println(myStack.Pop())
 	fmt.Println("count - ", myStack.Count())
 	fmt.Println(myStack.Pop())
-	fmt.Println("count - ", myStack.Count())*/
+	fmt.Println("count - ", myStack.Count())
 	var myQueue src.Queue
 	myQueue.Enqueue(1)
 	myQueue.Enqueue(2)
@@ -30,7 +30,10 @@ func main() {
 	fmt.Println("deq", myQueue.Dequeue())
 	fmt.Println("size", myQueue.Count())
 	fmt.Println("peek", myQueue.Peek())
-	/*var myDeque src.Deque
+	fmt.Println("deq", myQueue.Dequeue())
+	fmt.Println("deq", myQueue.Dequeue())
+	fmt.Println("deq", myQueue.Dequeue())
+	var myDeque src.Deque
 	myDeque.EnqueueFirst(1)
 	myDeque.EnqueueFirst(2)
 	myDeque.EnqueueFirst(3)
@@ -38,10 +41,16 @@ func main() {
 	myDeque.EnqueueFirst(5)
 	myDeque.EnqueueLast(6)
 	fmt.Println("DEQ FIRST: ", myDeque.DequeueFirst())
+	fmt.Println("SIZE: ", myDeque.Count())
 	fmt.Println("DEQ LAST: ", myDeque.DequeueLast())
+	fmt.Println("SIZE: ", myDeque.Count())
 	fmt.Println("DEQ FIRST: ", myDeque.DequeueFirst())
+	fmt.Println("SIZE: ", myDeque.Count())
 	fmt.Println("DEQ LAST: ", myDeque.DequeueLast())
+	fmt.Println("SIZE: ", myDeque.Count())
 	fmt.Println("DEQ FIRST: ", myDeque.DequeueFirst())
-	fmt.Println("DEQ FIRST: ", myDeque.DequeueFirst())*/
+	fmt.Println("SIZE: ", myDeque.Count())
+	fmt.Println("DEQ FIRST: ", myDeque.DequeueFirst())
+	fmt.Println("SIZE: ", myDeque.Count())
 
 }
